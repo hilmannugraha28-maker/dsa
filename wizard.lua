ae0147395a5db1b83a7a2207c14323c8c873266a
hilmannugraha28-maker <hilmannugraha28@gmail.com>
2026-05-17 19:18:23 +0700

    Update wizard.lua

--- wizard.lua
+++ wizard.lua
@@ -21,7 +21,7 @@ local S = {
     AutoRespawn=false, FarmRange=150, FarmDelay=0.3,
     OrbitRadius=4, OrbitSpeed=120, AttackRate=0.1,
     FarmOrigin=nil, LastPos=nil,
-    TargetHPMin=120, TargetHPMax=500, -- hanya serang mob MaxHP 120-500
+    TargetHPMin=120, TargetHPMax=1000, -- hanya serang mob MaxHP 120-1000
 }
 
 -- Save position history (rolling 1 menit)
